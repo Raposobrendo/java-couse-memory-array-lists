105d88a8ce6d31220d8c873a00c4e1c462c3ad30
Brendo Raposo <raposobrendo@gmail.com>
2026-01-23 22:45:36 -0300

    ArrayList Challenge Ended

--- src/Challenges/ChallengeArrayList.java
+++ src/Challenges/ChallengeArrayList.java
@@ -10,6 +10,9 @@ public class ChallengeArrayList {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        int id;
+        boolean idTaken;
+
         System.out.print("How many employees will be registered? ");
         int employeesQuantity = sc.nextInt();
 
@@ -17,8 +20,19 @@ public class ChallengeArrayList {
 
         for(int i = 0; i < employeesQuantity; i++){
             System.out.printf("Employee #%d\n", i+1);
-            System.out.print("ID: ");
-            int id = sc.nextInt();
+            do {
+                System.out.print("ID: ");
+                id = sc.nextInt();
+                idTaken = false;
+
+                for (ArrayListEmployees n : employees) {
+                    if (id == n.getId()) {
+                        System.out.println("ID already exists, select another one.");
+                        idTaken = true;
+                        break;
+                    }
+                }
+            } while (idTaken);
             for(ArrayListEmployees n : employees){
                 while(id == n.getId()){
                     System.out.println("ID already exists, select another one: ");
@@ -31,11 +45,31 @@ public class ChallengeArrayList {
             System.out.print("Salary: ");
             double salary = sc.nextDouble();
             employees.add(new ArrayListEmployees(id, name, salary));
-
+            System.out.println();
         }
 
+        boolean idExists = false;
+        System.out.println();
+        System.out.print("Enter the employee ID that will have salary increase:  ");
+        int idIncreaseSalary = sc.nextInt();
+        for(ArrayListEmployees n : employees){
+            if(idIncreaseSalary == n.getId()){
+                System.out.print("Enter percentage: ");
+                n.increaseSalary(sc.nextDouble());
+                idExists = true;
+                break;
+            }
+        }
 
+        if (!idExists){
+            System.out.println("This ID does no exist!");
+        }
 
+        System.out.println();
+        System.out.println("List of employees: ");
+        for(ArrayListEmployees n : employees){
+            System.out.println(n);
+        }
 
         sc.close();
     }
